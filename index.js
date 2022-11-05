c480f22d707d5ae0f772e70a40d9afce4b083fd8
Joshua Doud <bigjj3393@yahoo.com>
2022-11-05 16:00:18 -0700

    commit tree a49c4f7e46994dca6840e43ac4ea55a1b93c0366

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+  const seenNumbers = {};
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    if (seenNumbers[complement]) return true;
+    seenNumbers[array[i]] = true;
+  }
+  return false
+} 
 
 /* 
   Write the Big O time complexity of your function here
